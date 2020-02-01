79c77900b54dd8a4286e7534e401798cb8cfac7a
Silviu Miron <silviu.miron@tractorsetgo.com>
2020-02-01 23:54:04 +0200

    feat: Functional core

--- GameJam/Assets/Scripts/PuzzleLogics/Typ2LeverLogic.cs
+++ GameJam/Assets/Scripts/PuzzleLogics/Typ2LeverLogic.cs
@@ -4,41 +4,42 @@ using UnityEngine;
 
 public class Typ2LeverLogic : Logic
 {
-    public override void action()
-    {
-        if (panelHasElectricity)
-        {
-            gameObject.layer = 8;
-            foreach (Transform child in transform.parent)
-            {
-            if (!transform.GetComponent<Switch>().isTriggered)
-            {
-                if (child.name != this.name)
-                {
-                    Debug.Log("Found sibling " + child.name);
+	public override void action()
+	{
+		if (panelHasElectricity)
+		{
+			gameObject.layer = 8;
+			foreach (Transform child in transform.parent)
+			{
+				if (!transform.GetComponent<Switch>().isTriggered)
+				{
+					if (child.name != this.name)
+					{
+						Debug.Log("Found sibling " + child.name);
 
-                    if (child.GetComponent<Switch>().isTriggered)
-                    {
-                        child.GetComponent<Switch>().setTrigger(!child.GetComponent<Switch>().isTriggered);
-                        child.position = child.position - new Vector3(0, 1, 0);
-                    child.GetComponent<Switch>().setTrigger(false);
+						if (child.GetComponent<Switch>().isTriggered)
+						{
+							child.GetComponent<Switch>().setTrigger(!child.GetComponent<Switch>().isTriggered);
+							child.position = child.position - new Vector3(0, 1, 0);
+							child.GetComponent<Switch>().setTrigger(false);
 
-                    //child.position = child.position + new Vector3(0, 1, 0);
-                    //else
-                    //child.position = child.position - new Vector3(0, 1, 0);
+							//child.position = child.position + new Vector3(0, 1, 0);
+							//else
+							//child.position = child.position - new Vector3(0, 1, 0);
+						}
+					}
+				}
+				transform.GetComponent<Switch>().setTrigger(!transform.GetComponent<Switch>().isTriggered);
 
-                }
-                }
-            }
-            transform.GetComponent<Switch>().setTrigger(!transform.GetComponent<Switch>().isTriggered);
-
-            if (transform.GetComponent<Switch>().isTriggered)
-                transform.position = transform.position + new Vector3(0, 1, 0);
-            else
-                transform.position = transform.position - new Vector3(0, 1, 0);
-        } else
-        {
-            gameObject.layer = 0;
-        }
-    }
+				if (transform.GetComponent<Switch>().isTriggered)
+					transform.position = transform.position + new Vector3(0, 1, 0);
+				else
+					transform.position = transform.position - new Vector3(0, 1, 0);
+			}
+		}
+		else
+		{
+			gameObject.layer = 0;
+		}
+	}
 }
