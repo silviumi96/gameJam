fd5c929e204fc8253665451f99b872cfc7ccbe56
Muresan Paul <paulsummerblack@gmail.com>
2020-02-01 16:06:16 +0200

    added abstract logic class. LevelLogic & Type2LeverLogic extends the logic class and implements action() in their own specific game way

--- GameJam/Assets/LeverLogic.cs
+++ GameJam/Assets/LeverLogic.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LeverLogic : MonoBehaviour
+public class LeverLogic : Logic
 {
-
-    public void action()
+    public override void action()
     {
-        // do game
-        transform.GetComponent<Switch>().setTrigger(true);
+        transform.GetComponent<Switch>().setTrigger(!transform.GetComponent<Switch>().isTriggered);
 
         if (transform.GetComponent<Switch>().isTriggered)
-            transform.position = transform.position + new Vector3(1, 1, 1);
-
+            transform.position = transform.position + new Vector3(0, 1, 0);
+        else
+            transform.position = transform.position - new Vector3(0, 1, 0);
     }
 }
+
