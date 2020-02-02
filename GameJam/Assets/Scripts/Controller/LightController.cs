18920b28902cbe8604dac12f2f6b5defd4eb083e
Silviu Miron <silviu.miron@tractorsetgo.com>
2020-02-02 15:10:42 +0200

    feat: Add assets and wip room1

--- GameJam/Assets/Scripts/Controller/LightController.cs
+++ GameJam/Assets/Scripts/Controller/LightController.cs
@@ -4,29 +4,26 @@ using UnityEngine;
 
 public class LightController : Logic
 {
+	public List<GameObject> lights;
+	private bool isSwitched = false;
 
-    public List<GameObject> lights;
-    private bool isSwitched = false;
-
-    public override void action() 
-    {
-        if (!isSwitched)
-        {
-            foreach (var light in lights)
-            {
-                light.GetComponent<Light>().intensity = 4;
-            }
-            isSwitched = true;
-        }
-        else 
-        {
-            foreach (var light in lights)
-            {
-                light.GetComponent<Light>().intensity = 1;
-            }
-            isSwitched = false;
-        }
-    }
-
-    
+	public override void action()
+	{
+		if (!isSwitched)
+		{
+			foreach (var light in lights)
+			{
+				light.GetComponent<Light>().intensity = 4f;
+			}
+			isSwitched = true;
+		}
+		else
+		{
+			foreach (var light in lights)
+			{
+				light.GetComponent<Light>().intensity = 0.3f;
+			}
+			isSwitched = false;
+		}
+	}
 }
