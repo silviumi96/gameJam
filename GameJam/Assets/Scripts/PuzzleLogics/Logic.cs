e56b4f910a24ed6262d2315fb4166059290d7320
Ema Turcu <emagames@yahoo.com>
2020-02-02 13:59:13 +0200

    changed scripts part 1

--- GameJam/Assets/Scripts/PuzzleLogics/Logic.cs
+++ GameJam/Assets/Scripts/PuzzleLogics/Logic.cs
@@ -5,17 +5,7 @@ using UnityEngine;
 public abstract class Logic : MonoBehaviour
 {
     public bool panelHasElectricity;
-    public bool isActiveLogic = true;
-    // Start is called before the first frame update
-    public void doAction()
-    {
-        if (isActiveLogic)
-        {
-            action();
-        }
-    }
 
-	// Start is called before the first frame update
 	public virtual void action() { }
 
 	public virtual void action(Transform tran)
--- GameJam/Assets/Scripts/PuzzleLogics/PipeLogic.cs
+++ GameJam/Assets/Scripts/PuzzleLogics/PipeLogic.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PipeLogic : Logic
 {
     public GameObject water;
+    public GameObject powerPannel3;
 
     public override void action()
     {
@@ -13,12 +14,31 @@ public class PipeLogic : Logic
             transform.GetComponent<Switch>().setTrigger(!transform.GetComponent<Switch>().isTriggered);
 
             if (transform.GetComponent<Switch>().isTriggered)
+            {
                 water.GetComponent<WaterLogic>().setWaterLevel(0);
+                foreach (Transform child in powerPannel3.transform)
+                {
+                    child.GetComponent<Logic>().setPanelHasElectricity(true);
+                    child.gameObject.layer = 8;
+                }
+            }
             else
+            {
                 water.GetComponent<WaterLogic>().setWaterLevel(0.5f);
+                foreach (Transform child in powerPannel3.transform)
+                {
+                    child.GetComponent<Logic>().setPanelHasElectricity(false);
+                    child.gameObject.layer = 0;
+                }
+            }
         } else
         {
             water.GetComponent<WaterLogic>().setWaterLevel(1);
+            foreach (Transform child in powerPannel3.transform)
+            {
+                child.GetComponent<Logic>().setPanelHasElectricity(true);
+                child.gameObject.layer = 8;
+            }
             // TODO: handle death
         }
     }
